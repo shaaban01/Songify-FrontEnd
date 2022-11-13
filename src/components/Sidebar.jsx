4134fa055b14f03102a0b9ea5264477d5388b48a
shaaban01 <91268135+shaaban01@users.noreply.github.com>
2022-11-13 19:38:51 +0200

    logo added + finish the api endpoint

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#2c0e41]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <img src={logo} alt="logo" className="w-full h-40 object-contain" />
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
@@ -48,7 +48,7 @@ const Sidebar = () => {
           mobileMenuOpen ? 'left-0' : '-left-full'
         }`}
       >
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <img src={logo} alt="logo" className="w-full  h-40 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
